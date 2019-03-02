67a8ea83f986c69207fc85ccef1d7dea785cb552
sumaili <48146275+sumaili@users.noreply.github.com>
2019-03-02 11:39:45 +0300

    Add files via upload

---
+++ App2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop2_coursework_2;
+
+/**
+ *
+ * @author Guellord de Jesus
+ */
+public class App2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
